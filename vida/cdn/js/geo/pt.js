52f31cdfcbed8e0543e4592e1c43eeb4f40443a1
supmedba <62884773+supmedba@users.noreply.github.com>
2020-05-13 16:21:38 +0400

    1

---
+++ vida/cdn/js/geo/pt.js
@@ -0,0 +1,19 @@
+(function() {
+    'use strict';
+
+    window.countryList = {
+        ph: {
+            name: 'Pilipinas',
+            code: 'ph',
+            price: 1990,
+            oldPrice: 3980,
+            labelPrice: 'php',
+            phoneHelper: 'Halimbawa, +63 123456789',
+            nameHelper: 'Ramon Barb',
+            nameError: 'Pangalan (kailangan)',
+            phoneError: 'Hindi tamang nakasaad ang bilang',
+            countryError: 'Bansa (kailangan)',
+            isDefault: true
+        }
+    };
+})();
